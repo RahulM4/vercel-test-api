c4f3ade1ae832cbf000c7323d4285fd9409edc1c
RAHUL MAHTO <rahulrps447@gmail.com>
2023-10-25 19:14:14 +0530

    updated bakend url

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://vercel-test-api-xi.vercel.app";
   const initialNotes = []
   const [notes, setNotes] = useState(initialNotes);
 
